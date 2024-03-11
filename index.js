4208c9b19c20d6db3fd3d5a48580bf8dc3bab39f
Benjamin Chaillan <benjamin.chaillan-pro@laposte.net>
2024-03-11 20:09:52 +0100

    all square

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+
+// le canvas prend toute la taille disponible sur le navigateur
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+// Carré remplie
+context.fillRect(25, 25, 100, 100);
+
+// Efface la zone spécifiée
+context.clearRect(45, 45, 60, 60);
+
+// Dessiné un contour
+context.strokeRect(50, 50, 50, 50);
