7462a9dd403a8115089897b1b4abd9dba3ea4958
Benjamin Chaillan <benjamin.chaillan-pro@laposte.net>
2024-03-13 20:23:43 +0100

    add axes

--- index.js
+++ index.js
@@ -6,10 +6,59 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 // Carré remplie (x, y, width, height)
-context.fillRect(25, 25, 100, 100);
+context.fillRect(100, 100, 100, 100);
 
-// // Efface la zone spécifiée
-// context.clearRect(45, 45, 60, 60);
+// Efface la zone spécifiée
+context.clearRect(120, 120, 60, 60);
 
-// // Dessiné un contour
-// context.strokeRect(50, 50, 50, 50);
+// Dessiné un contour
+context.strokeRect(125, 125, 50, 50);
+
+// AXES X ET Y
+context.beginPath();
+context.moveTo(40, 40);
+context.lineTo(innerWidth, 40);
+context.closePath();
+context.stroke();
+
+context.beginPath();
+context.moveTo(300, 25);
+context.lineTo(350, 40);
+context.closePath();
+context.stroke();
+
+context.beginPath();
+context.moveTo(300, 55);
+context.lineTo(350, 40);
+context.closePath();
+context.stroke();
+
+context.beginPath();
+context.moveTo(25, 300);
+context.lineTo(40, 350);
+context.closePath();
+context.stroke();
+
+context.beginPath();
+context.moveTo(55, 300);
+context.lineTo(40, 350);
+context.closePath();
+context.stroke();
+
+context.beginPath();
+context.moveTo(40, 40);
+context.lineTo(40, innerHeight);
+context.closePath();
+context.stroke();
+
+context.font = '25px serif';
+context.strokeText('X', 200, 25);
+
+context.font = '25px serif';
+context.strokeText('Y', 10, 250);
+
+context.font = '25px serif';
+context.strokeText('0', 50, 35);
+
+context.font = '25px serif';
+context.strokeText('0', 20, 70);
